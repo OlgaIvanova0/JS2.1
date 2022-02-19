edcb0791c3dedc24031672b2530dfb4dd7a79ab6
OlgaIvanova0 <88612307+OlgaIvanova0@users.noreply.github.com>
2022-02-19 15:22:31 +0500

    готово 2 задание

--- Menu.js
+++ Menu.js
@@ -14,4 +14,46 @@ class Menu {
         result += `</ul>`;
         return result
     }
-}
+   
+};
+
+class Button {
+    constructor(id, className, buttonText){
+        this.id = id;
+        this.className = className;
+        this.buttonText = buttonText;
+    }
+    getButton(){
+        let button = `<button class="${this.className}" id="${this.id}">${this.buttonText}</button>`;
+        return button;
+
+    }
+    clickButton(){
+        let button = document.getElementById(`${this.id}`);
+        button.addEventListener('click',() => {
+            const menu = document.querySelectorAll('li');
+            for (let i = 0; i < menu.length; i++){
+                menu[i].remove();
+            };
+        });
+    }
+};
+
+class SubMenu extends Menu{
+    clickSubMenu(){
+        const elems = document.querySelectorAll(`li`);
+        for(let i = 0; i< elems.length; i++) {
+            let a = elems[i].querySelector('a').cloneNode(true);
+            console.log(a);
+            elems[i].addEventListener ('click', () =>{
+                elems[i].innerHTML = this.render();
+                const refEl = elems[i].querySelector('ul');
+                elems[i].insertBefore(a, refEl);  
+            });
+            
+        };
+    };
+};
+
+
+    
